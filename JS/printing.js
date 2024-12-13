fca48499d5c63d17ff3ffa1e1c9bb5db1ca83ca2
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2024-12-13 22:04:56 +0530

    datatypes

---
+++ JS/printing.js
@@ -0,0 +1,27 @@
+console.log("coffee")
+process.stdout.write("with sam")
+
+/*
+DATA TYPES : 
+-string
+-number
+-boolean
+-Bigint
+-undefined
+-null
+-objects
+-symbol
+*/
+
+var score = 122
+let score = 102
+// what is the difference btween the let and var ?
+
+let name = "sam"
+let loggedin = false
+
+let teatypes = ['pepsi','cola','dew','sprite']
+let user = {firstname : "gourav", lastname: "anand"}
+
+
+//const vs let
