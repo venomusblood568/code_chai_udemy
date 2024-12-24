16ce66cf64456b411fb8ec2258bf4e46a59b9ec0
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2024-12-24 18:32:12 +0530

    healthcheck routes

--- Backend/vidtube/src/app.js
+++ Backend/vidtube/src/app.js
@@ -14,4 +14,12 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+//import Routes
+import healthcheckerRouter from "./routes/healthcheck.routes.js"
+
+//routes
+app.use("/api/v1/healthcheck",healthcheckerRouter)
+
+
 export { app };
---
+++ Backend/vidtube/src/routes/healthcheck.routes.js
@@ -0,0 +1,12 @@
+import {Router} from "express";
+import { healthCheck } from "../controllers/healthcheck.controller.js";
+
+const router = Router()
+
+router.route("/").get(healthCheck);
+
+/*
+Use export default when there is a single primary value or object that you want to export from a module.
+Use export {} for named exports when you want to export multiple values or give the user control over what to import.
+*/
+export default router
