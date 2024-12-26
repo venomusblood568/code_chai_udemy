c97a97bbf107d32b29fe8a55e08b6517ccee0b5f
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2024-12-26 23:27:07 +0530

    upload img and coverImg

--- Backend/vidtube/src/app.js
+++ Backend/vidtube/src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser"
 
 const app = express();
+
 
 app.use(
   cors({
@@ -14,6 +16,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 //import Routes
 import healthcheckerRouter from "./routes/healthcheck.routes.js"
