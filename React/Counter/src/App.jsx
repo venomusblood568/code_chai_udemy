85525104873addd7336b8b3f74592b63749011da
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-01-03 10:35:28 +0530

    counter finished

--- React/Counter/src/App.jsx
+++ React/Counter/src/App.jsx
@@ -1,21 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const[count, setCounter] = useState(0)
+  const [count, setCounter] = useState(0);
 
   const addValue = () => {
-    setCounter(count + 1)
-  }
+    setCounter(count + 1);
+  };
 
   const removeValue = () => {
-    setCounter(count - 1)
-  }
+    setCounter(count - 1);
+  };
 
   const reset = () => {
-    setCounter(0)
-  }
+    setCounter(0);
+  };
+
+  const [inputValue,setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addValueFromInput = () => {
+    const addValueTo = Number(inputValue);
+    if(!isNaN(addValueTo)) {
+      setCounter(count + addValueTo);
+    }
+    setInputValue("");
+  };
 
   return (
     <>
@@ -24,8 +37,17 @@ function App() {
       <button onClick={addValue}>Add value</button>{" "}
       <button onClick={removeValue}>remove value</button>{" "}
       <button onClick={reset}>Reset counter</button>
+      <div style={{ marginTop: "10px" }}>
+        <input
+          type="text"
+          placeholder="Enter a value to add"
+          value= {inputValue}
+          onChange={handleInputChange}
+        ></input>{" "}
+        <button onClick={addValueFromInput}>Add to Count</button>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
